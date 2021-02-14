3ae86e5f72a1f2736041d96287ad156b24c38827
Imanol-Montero <20197920@itla.edu.do>
2021-02-13 22:26:39 -0400

    cambio

--- Progrma1.cs
+++ Progrma1.cs
@@ -6,7 +6,7 @@ namespace Programa_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Hola tMundo!");
         }
     }
 }
